227f438c98b73478fc5373d761c15189ac90fe32
Ömer <62448865+karakayaommer@users.noreply.github.com>
2022-03-01 23:08:35 +0300

    Add files via upload

---
+++ AspNet_009_UyariMesajKutusu/IkinciSayfa.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNet_009_UyariMesajKutusu
+{
+    public partial class IkinciSayfa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AspNet_009_UyariMesajKutusu/IlkSayfa.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNet_009_UyariMesajKutusu
+{
+    public partial class IlkSayfa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+protected void Button1_Click(object sender, EventArgs e)
+{
+    Response.Write("<script lang='JavaScript'>alert('Uyarı Mesajı'); window.location = 'IkinciSayfa.aspx'</script>");
+}
+    }
+}
